b5ff4b688a587c8e62859471b5bb358ee57c2a96
Twigonometry <mac.j.goodwin@gmail.com>
2023-07-03 20:07:45 +0100

    Adding docker installs so the source works

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh
 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce
+
 zsh ./go.zsh
