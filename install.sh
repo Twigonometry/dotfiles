8b4682e298a7129f7619cd26e2c8b9c1d72b6664
Twigonometry <mac.j.goodwin@gmail.com>
2023-07-27 10:58:15 +0100

    Fix Ubuntu install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+sudo apt install zsh
+chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s /usr/bin/zsh
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
